3d3c74c89b0c82da68e0c79c2e919122d050f174
Aimene <aimennouri65@gmail.com>
2020-03-27 19:58:23 +0100

    add title and footer

--- weatherapp/src/App.vue
+++ weatherapp/src/App.vue
@@ -1,18 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Weather App</h1>
+    <footer class="page-footer">
+  <div class="container">
+    <small>Made with <span>❤</span> by <a href="#" target="_blank">Aimene Nouri</a>
+    </small>
+  </div>
+  </footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'app',
 }
 </script>
 
@@ -24,5 +24,39 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+/* FOOTER
+–––––––––––––––––––––––––––––––––––––––––––––––––– */
+.page-footer {
+  text-align: center;
+  font-size: 22px;
+  margin-top: 650px;
+  bottom: 0;
+}
+
+.page-footer span {
+  color: red;
+}
+.page-footer a {
+  text-decoration: none;
+  color: #2c3e50;
+  position: relative;
+   bottom: 0;
+  left: 0;
+  right: 0;
+  }
+.page-footer a:after {
+  content: "";
+  position: absolute;
+  z-index: -1;
+  top: 60%;
+  left: -0.1em;
+  right: -0.1em;
+  bottom: 0;
+  transition: top 200ms cubic-bezier(0, .8, .13, 1);
+  background-color: rgba(19, 187, 181, 0.502);
+}
+.page-footer a:hover:after {
+  top: 0%;
 }
 </style>
